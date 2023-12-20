f692a6f9d1ec686f1a0db2aaf78ccbb10aa37341
Pratkshya <147973972+Pratkshya@users.noreply.github.com>
2023-12-20 22:56:06 +0545

    Add files via upload

---
+++ Ternary_operators.cpp
@@ -0,0 +1,17 @@
+//Ternary operators
+
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int gifts = 5;
+	int people = (gifts ==5) ? people = 2 : people = 4;
+	cout<<"No of ppl are " << people;
+	
+	return 0;
+}
+ /*
+ int a = (var == some condition)? a = right value gets printed : a = wrong value gets printed
+ 
+ 
+ */
