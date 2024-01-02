2bbdee0d7e68719b2ed230f5ae7e84a800221ea5
Pratkshya <147973972+Pratkshya@users.noreply.github.com>
2024-01-02 08:03:18 +0545

    Add files via upload

---
+++ Say_my_name.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string name;   //name should always be declared.
+	cout << "Enter your name :" <<endl;
+	cin >> name;
+	
+	cout << "Hello "<< name << endl;
+	cout << "Pleasure meeting you.";
+	
+	
+	return 0;
+}
